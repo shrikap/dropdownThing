43798f7e98f329b2992b9cfb910379bf0de63142
shrikap <shrikapunuganti@gmail.com>
2023-07-18 10:19:46 -0700

    Initial Commit

---
+++ dropdownThing/dropdownThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropdownThingApp.swift
+//  dropdownThing
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct dropdownThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
